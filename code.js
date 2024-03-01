db6e1411b08b12fb76bbb52c0d76a65df0b72385
Abalouan <161030757+Abalouan@users.noreply.github.com>
2024-03-01 10:35:03 -0300

    Update code.js

--- code.js
+++ code.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const texto = document.body.querySelector("#texto");
 
     texto.addEventListener("click", () => {
-        if(texto.textContent==="Ola mundo")
+        if(texto.textContent==="ola mundo")
         texto.textContent="Hello World";
     else
-        texto.textContent="Ola mundo";
+        texto.textContent="ola mundo";
     });
 });
 
